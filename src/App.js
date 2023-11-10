fa174fdc7cc12c38da4b2a566a9ea1e23d0dfa2e
Zayon Nieves <zayon.nieves1@gmail.com>
2023-11-09 22:12:16 -0500

    added menu section with items

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
-import './css/header.css'
-import './css/about.css'
-import FrozenCowIMG from '../src/assets/fcc.jpg'
+import './css/header.css';
+import './css/about.css';
+import './css/menu.css';
+import FrozenCowLogo from './assets/fcc_logo.jpg';
+import specialtyItems from './menuItems.js';
 
 function App() {
   return (
@@ -12,23 +14,77 @@ function App() {
 
         <nav className='flexbox-container row'>
 
-          <a href='#about'>About</a>
+          <a>About</a>
           <a> | </a>
-          <a href='#menu'>Menu</a>
+          <a>Menu</a>
           <a> | </a>
-          <a href='#'>Contact</a>
+          <a>Contact</a>
           <a> | </a>
-          <a href='#'>Careers</a>
+          <a>Careers</a>
 
         </nav>
       </header>
 
       <div className='about flexbox-container'>
-       <h1 className='handmade-and-wholesome'>Handmade and Wholesome</h1>
+        
+        <div className='company-logo'>
+          <img src={FrozenCowLogo} alt='frozen cow logo' />
+        </div>
+
+        <div className='motto-banner'>
+          <h2>Handmade and Wholesome</h2>
+        </div>
+
       </div>
 
       <div className='menu flexbox-container'>
-        <h1>Menu</h1>
+        
+        <div className='menu-title'>
+          <h2>Menu</h2>
+        </div>
+
+        <div className='specialty'>
+          <h3>Specialty</h3>
+        </div>
+
+        <div className='menu-items grid-container flexbox-container'>
+
+          {specialtyItems.map((item, index) => {
+
+            // return a div element for each menu item
+            return (
+              <div key={index} className='grid-item'>
+                <p className='menu-item'><strong>{item.name}</strong></p>
+                <p>{item.description}</p>
+              </div>
+            );
+          })}
+
+        </div>
+
+        <div className='flavors-and-tossins'>
+          <h2>Choose Your Flavors & Toss-ins</h2>
+
+          <h3>Flavors</h3>
+          <p>
+            Sweet Cream, Vanilla, Chocolate, Dark Chocolate, Cherry, Coffee, Cinnamon, Almond, Amaretto, Irish Crean, Mint, Rum,<br />
+            Orange, Lemon, Cake Batter, Cheesecake, Cotton Candy, Dulce de Leche, Peanut Butter, Peach
+          </p>
+
+         <h3>Toss-ins ($.95)</h3>
+         <p>
+          <strong>Fruits:</strong> Banana, Peach, Ple Cherries, Black Cheries, Pineapple, Strawberry Blueberry, Raspberry, Mango, Raisins<br /><br />
+          <strong>Nuts: </strong> Peanuts, Walnuts, Almonds, Pecans, Pistachios<br /><br />
+          <strong>Sweets:</strong> Sprinkles, Gummy Bears, Chocolate Chips, White Chocolate Chips, M&M's, Peanut Butter Cups, Butteringer, Heath Bar,<br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Coconut Marshmallow, Marshmallow Flut, Nutella, Cinnamon Chips, Toasted Coconut<br /><br />
+          <strong>Crumbles:</strong> Cookie Dough, Brownie, Birthday Cake, Carot Cake, Graham Crackers, Graham Cracker Crumbs,<br />
+          &nbsp;&nbsp;&nbsp;&nbsp;Oreos, Pretzels, Nila Waters, Lady Fingers, Sea Salt<br /><br />
+          <strong>Sauces & Syrups:</strong> Fudge, Caramel, Praline, Peanut Butter, Honey, Maple<br /><br />
+          <strong>Premium Toss-ins ($1.00):</strong> Cheesecake, Matcha Green Tea
+         </p>
+        </div>
+
+
       </div>
 
     </div>
---
+++ src/menuItems.js
@@ -0,0 +1,139 @@
+
+
+/**
+ * File contains menu items represented as Objects (Separation of Concerns)
+ */
+
+const specialtyItems = [ 
+
+    // Specialty Items
+
+    {
+        name: 'Apple Pie A La Mode',
+        description: 'Cinnamon, Apple Pie Filling, Pie Crust, Caramel, Whip Cream, Dash of Cinnamon'
+    }, 
+    {
+        name: 'Bananas Foster',
+        description: 'Bananas, Praline, Rum Extract Caramel, Whip Cream'
+    }, 
+    {
+        name: 'Banana Pudding',
+        description: 'Vanilla Ice Cream, Banana, Nila Wafers, Whip Cream'
+    },
+    {
+        name: 'Banana Split',
+        description: 'Bananas, Strawberries, Pineapple, Topped with Fudge, Whip Cream & Maraschino Cherry'
+    },
+    {
+        name: 'Birthday Cake',
+        description: 'Funfetti Cake, Frosting, Cake Batter, Sprinkles'
+    },
+    {
+        name: 'Brownie Treat',
+        description: 'Walnuts, Brownies, Fudge Caramel'
+    },
+    {
+        name: 'Cannoli',
+        description: 'Chocolate Chips, Cannoli Filling, Cannoli Shell Crumbles, Whip Cream'
+    },
+    {
+        name: 'Carrot Cake',
+        description: 'Frosting, Carrot Cake, Whip Cream, Cinnamon Sprinkle'
+    },
+    {
+        name: "Cassandra's Cherry Cordial",
+        description: 'Cherry Ice Cream, Chocolate Chips, Black Cherries, Fudge Drizzle'
+    },
+    {
+        name: 'Cherry Cheesecake',
+        description: 'Cheesecake, Pie Cherries, Graham Cracker Crumbs, Whip Cream'
+    },
+    {
+        name: 'Cherry Pie A La Mode',
+        description: 'Cherry Pie Filling, Pie Crust, Whip Cream, Maraschino Cherry'
+    },
+    {
+        name: 'Chocolate in Your Peanut Butter',
+        description: 'Peanut Butter Ice Cream, Chocolate Chips, Peanut Butter, Fudge Drizzle'
+    },
+    {
+        name: 'Cotton Candy Cloud',
+        description: 'Cotton Candy Ice Cream, White Chocolate Chips, Marshmallows, Whip Cream, Sprinkles'
+    },
+    {
+        name: 'Cow Tracks',
+        description: 'Fudge, Peanut Butter Cups, Fudge Drizzle'
+    },
+    {
+        name: 'Hot Chocolate',
+        description: 'Chocolate Ice Cream, Chocolate Chips, Marshmallow, Whip Cream'
+    },
+    {
+        name: 'Joyful Almond',
+        description: 'Chocolate Chips, Almond, Coconut Flakes, Topped with Toasted Coconut Flakes'
+    },
+    {
+        name: 'Key Lime Cheesecake',
+        description: 'Cheesecake, Key Lime Juice, Graham Cracker Crumbs, Whip Cream'
+    },
+    {
+        name: 'Key Lime Pie',
+        description: 'Key Lime Juice, White Chocolate Chips, Graham Cracker Crumbs, Whip Cream'
+    },
+    {
+        name: 'Lemon Pie',
+        description: 'Lemon Juice, White Chocolate Chips, Graham Cracker Crumbs, Whip Cream'
+    },
+    {
+        name: 'Matcha Coconut',
+        description: 'Matcha Green Tea, Coconut Cream, Coconut Flakes'
+    },
+    {
+        name: 'Peanut Butter Pretzel Chip',
+        description: 'Chocolate Chips, Peanut Butter, Pretzels'
+    },
+    {
+        name: 'Pina Colada',
+        description: 'Rum Extract, Pineapple, Coconut Flakes, Whip Cream, Topped with Toasted Coconut Flakes'
+    },
+    {
+        name: 'Raspberry Chip Fudge',
+        description: 'Raspberries, Chocolate Chips, Fudge'
+    },
+    {
+        name: 'Raspberry Delight',
+        description: 'Raspberries, Brownies, Whip Cream, Fudge Drizzle'
+    },
+    {
+        name: 'Rockier Road',
+        description: 'Chocolate Ice Cream, Marshmallows, Almonds, Caramel'
+    },
+    {
+        name: "S'mores",
+        description: 'Chocolate Ice Cream, Chocolate Chips, Graham Crackers, Marshmallow Fluff'
+    },
+    {
+        name: 'Strawberry Cheesecake',
+        description: 'Cheesecake, Strawberries, Graham Cracker Crumbs, Whip Cream'
+    },
+    {
+        name: 'Strawberry Shortcake',
+        description: 'Shortcake, Strawberries, Whip Cream'
+    },
+    {
+        name: 'Tiramisu',
+        description: 'Tiramisu Cream, Coffee, Lady Fingers, Whip Cream, Cocoa Sprinkle'
+    },
+    {
+        name: 'Triple Berry',
+        description: 'Strawberries, Blueberries, Raspberries, Whip Cream'
+    },
+    {
+        name: 'Turtle',
+        description: 'Pecans, Fudge, Caramel'
+    }
+];
+
+
+// export data to be used in App.js
+export default specialtyItems;
